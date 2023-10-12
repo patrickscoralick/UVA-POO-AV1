90f18000453c58b8ee3d7a07408cbf57b912b2ab
Filipi <amoedofilipi@gmail.com>
2023-10-12 13:21:06 -0300

    hashCode implementado

--- sistemaA1POO/src/Models/Order.java
+++ sistemaA1POO/src/Models/Order.java
@@ -6,6 +6,7 @@ package Models;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  *
@@ -28,5 +29,18 @@ public class Order implements Serializable{
         this.products = products;
         this.isPromotion = isPromotion;
         this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return id == order.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
